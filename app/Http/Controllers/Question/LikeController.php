f40603388c5fb9f448e9d24b89d5d27cb4c2b251
Thales <thales.carrascal@gmail.com>
2023-12-27 16:02:30 -0300

    hey-13: adding global function file

--- app/Http/Controllers/Question/LikeController.php
+++ app/Http/Controllers/Question/LikeController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers\Question;
 
 use App\Http\Controllers\Controller;
-use App\Models\{Question, Vote};
+use App\Models\{Question, User};
 use Illuminate\Http\RedirectResponse;
 
 class LikeController extends Controller
 {
     public function __invoke(Question $question): RedirectResponse
     {
-        auth()->user()->like($question);
+        user()->like($question);
 
         return back();
     }
